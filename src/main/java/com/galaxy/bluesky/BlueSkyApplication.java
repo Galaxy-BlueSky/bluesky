56771f4177087dc9ef7247210d2aa6368ad4aeb4
Galaxy-BlueSky <silvergold79@naver.com>
2024-04-26 15:38:28 +0900

    init

--- src/main/java/com/galaxy/bluesky/BlueSkyApplication.java
+++ src/main/java/com/galaxy/bluesky/BlueSkyApplication.java
@@ -9,39 +9,39 @@ import java.util.List;
 @SpringBootApplication
 public class BlueSkyApplication {
 
-public static void main(String[] args) {
+  public static void main(String[] args) {
 
-  SpringApplication.run(BlueSkyApplication.class, args);
+    SpringApplication.run(BlueSkyApplication.class, args);
 
-  System.out.println("Hello World");
+    System.out.println("Hello World");
 
-  List<String> booklist = initializeBookList();
+    List<String> booklist = initializeBookList();
 
-  printBookList(booklist);
+    printBookList(booklist);
 
-  // 구구단 계산식
+    // 구구단 계산식
 
-}
+  }
 
-private static List<String> initializeBookList() {
+  private static List<String> initializeBookList() {
 
-  List<String> books = new ArrayList<>();
+    List<String> books = new ArrayList<>();
 
-  books.add("book1");
-  books.add("book2");
-  books.add("book3");
-  books.add("book4");
-  books.add("book5");
-  books.add("book6");
+    books.add("book1");
+    books.add("book2");
+    books.add("book3");
+    books.add("book4");
+    books.add("book5");
+    books.add("book6");
 
-  return books;
+    return books;
 
-}
+  }
 
-private static void printBookList(List<String> list) {
+  private static void printBookList(List<String> list) {
 
-  System.out.println(list);
+    System.out.println(list);
 
-}
+  }
 
 }
